b89c817cbf01ae29823b0368d68e37b990384ed6
kapsepratik <151921342+kapsepratik@users.noreply.github.com>
2024-06-06 10:29:50 +0530

    Add files via upload

---
+++ arrselectionsort.java
@@ -0,0 +1,45 @@
+package Arrys;
+
+import java.io.*;
+public class arrselectionsort
+{
+    void sort(int arr[])
+    {
+        int n = arr.length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            // Swap found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // for Prints
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+
+   
+    public static void main(String args[])
+    {
+        arrselectionsort ob = new arrselectionsort();
+        int arr[] = {-1,25,56,487,3,55,43,20};
+        ob.sort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
+}
